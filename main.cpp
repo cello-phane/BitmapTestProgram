417d9a9ba8db2f6be9525716fbb6c3f0195d5ebf
cello-phane <mled1986@yahoo.com>
2022-12-20 14:01:11 -0800

    Comments updated and origin is drawn with midpoint radius expansion

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #define WIN32_LEAN_AND_MEAN
 #define UNICODE
-#include <cstdlib>
 #include <windows.h>
+// #include <cstdlib>
 // #include <wingdi.h>//Don't include this directly, since windows.h implicitly includes that header
-#include <stdint.h>
+// #include <stdint.h>
 #include <string>
-#include <iostream>
 #include <cassert>
 typedef uint32_t u32;
 
@@ -34,11 +33,11 @@ int ClientHeight;
 //Vector for the x,y
 //The type of vec can be float,int, or double
 template<typename T>
-struct Vec2
+struct Point
 {
-  Vec2() : x(T(0)), y(T(0)) {}
-  Vec2(const T &x_) : x(x_), y(x_) {}
-  Vec2(T x_, T y_) : x(x_), y(y_) {}
+  Point() : x(T(0)), y(T(0)) {}
+  Point(const T &x_) : x(x_), y(x_) {}
+  Point(T x_, T y_) : x(x_), y(y_) {}
 public:
   T getX() const
   {
@@ -56,6 +55,8 @@ public:
   {
     y = y_;
   }
+  //This converts the input ClientWindows coordinates to normal cartesian coords
+  //TODO: maybe add an option to specify if initial Bitmap Height is negative or positive ( 0,0 is either at bot left or top left )
   void set_ndc(T& offset_x, T& offset_y, T& cell_size){
     x = x >= 0 ? offset_x + x * cell_size : offset_x - abs(x) * cell_size;
     y = y >= 0 ? offset_y + y * cell_size : offset_y - abs(y) * cell_size;
@@ -81,8 +82,8 @@ void DrawPixel(int X, int Y, u32& Color) {
 //Draws the lines parallel and perpendicular from top-left corner and bot-right corner points
 // Coordinates passed are the top left xy and bot right xy
 template<typename coordType>
-void OutlineSquare(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b, u32& Color){
-  auto xa = Vec_a.getX(), ya = Vec_a.getY(), xb = Vec_b.getX(), yb = Vec_b.getY();
+void OutlineSquare(Point<coordType>& Point_a, Point<coordType>& Point_b, u32& Color){
+  auto xa = Point_a.getX(), ya = Point_a.getY(), xb = Point_b.getX(), yb = Point_b.getY();
   for(auto x = xa;x < xb;++x){
     DrawPixel(x, yb, Color); //Horiz bottom
     DrawPixel(x, ya, Color); //Horiz top
@@ -95,8 +96,8 @@ void OutlineSquare(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b, u32& Color){
 
 //Draws pixel points on each corner point(vertex)
 template<typename coordType>
-void VertexPointSquare(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b, u32& Color){
-  auto xa = Vec_a.getX(), ya = Vec_a.getY(), xb = Vec_b.getX(), yb = Vec_b.getY();
+void VertexPointSquare(Point<coordType>& Point_a, Point<coordType>& Point_b, u32& Color){
+  auto xa = Point_a.getX(), ya = Point_a.getY(), xb = Point_b.getX(), yb = Point_b.getY();
   DrawPixel(xb - xa, ya, Color);      //top left point
   DrawPixel(xb, ya, Color);           //top right point
   DrawPixel(xb - xa, yb, Color);      //bot left point
@@ -105,8 +106,8 @@ void VertexPointSquare(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b, u32& Colo
 
 //Fills the area inside the coordinates
 template<typename coordType>
-void FillSquare(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b, u32& Color, std::string&& dir="right"){
-  auto xa = Vec_a.getX(), ya = Vec_a.getY(), xb = Vec_b.getX(), yb = Vec_b.getY();
+void FillSquare(Point<coordType>& Point_a, Point<coordType>& Point_b, u32& Color, std::string&& dir="right"){
+  auto xa = Point_a.getX(), ya = Point_a.getY(), xb = Point_b.getX(), yb = Point_b.getY();
   for(auto x = xa;x <= xb;++x){
     for(auto y = ya;y <= yb;++y){
       DrawPixel(x, y, Color); //Right Horiz
@@ -115,23 +116,23 @@ void FillSquare(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b, u32& Color, std:
 }
 
 void HLine_expand(int& xa, int& ya, int& radius, u32& Color) {
-  for(auto x=1;x<radius;++x){
+  for(auto x=1;x<radius/2;++x){
     DrawPixel(xa+x, ya, Color);
     DrawPixel(xa-x, ya, Color);
   }
 }
 
 void VLine_expand(int xa, int ya, int& radius, u32& Color){
-  for(auto y=1;y<radius;++y){
+  for(auto y=1;y<radius/2;++y){
     DrawPixel(xa, ya+y, Color);
     DrawPixel(xa, ya-y, Color);
   }
 }
 
 template<typename coordType>
-void DiagonalLine(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b,
+void DiagonalLine(Point<coordType>& Point_a, Point<coordType>& Point_b,
                   u32& Color, const std::string& dir){
-  auto xa = Vec_a.getX(), ya = Vec_a.getY(), xb = Vec_b.getX(), yb = Vec_b.getY();
+  auto xa = Point_a.getX(), ya = Point_a.getY(), xb = Point_b.getX(), yb = Point_b.getY();
   if(dir == "back" || dir == "downward"){
     /* /  */
     for(auto dx=xa,dy=yb;dx<xb && dy>ya;++dx,--dy){
@@ -148,14 +149,14 @@ void DiagonalLine(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b,
 
 //xa,ya and xb,yb are the top point and bottom point, that are adjacent to the right angle
 template<typename coordType>
-void OutlineRightTriangle(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b,
+void OutlineRightTriangle(Point<coordType>& Point_a, Point<coordType>& Point_b,
                           u32& Color, bool vflip=false, bool hflip=false){
-  auto xa = Vec_a.getX(), ya = Vec_a.getY(), xb = Vec_b.getX(), yb = Vec_b.getY();
+  auto xa = Point_a.getX(), ya = Point_a.getY(), xb = Point_b.getX(), yb = Point_b.getY();
   if((vflip == true && hflip == false) || (vflip == false && hflip == true)) {
-    DiagonalLine<coordType>(Vec_a, Vec_b, Color, "back");/* /| */
+    DiagonalLine<coordType>(Point_a, Point_b, Color, "back");/* /| */
   }
   else{
-    DiagonalLine<coordType>(Vec_a, Vec_b, Color, "front");/* |\ */
+    DiagonalLine<coordType>(Point_a, Point_b, Color, "front");/* |\ */
   }
  for(auto dx=xa, dy=ya;dx < xb && dy < yb;++dy,++dx){
     if(vflip == false){
@@ -177,28 +178,28 @@ void OutlineRightTriangle(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b,
 }
 
 template<typename coordType>
-void OutlineEquilTriangle(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b,
+void OutlineEquilTriangle(Point<coordType>& Point_a, Point<coordType>& Point_b,
                           u32& Color, bool vflip=false){
-  auto xa = Vec_a.getX(), ya = Vec_a.getY(), xb = Vec_b.getX(), yb = Vec_b.getY();
-  DiagonalLine<coordType>(Vec_a, Vec_b, Color, "back");
-  Vec2<coordType> Vec_c {xa+(xb-xa), ya};
-  Vec2<coordType> Vec_d {xb+(xb-xa), yb};
-  DiagonalLine<coordType>(Vec_c, Vec_d, Color, "front");
+  auto xa = Point_a.getX(), ya = Point_a.getY(), xb = Point_b.getX(), yb = Point_b.getY();
+  DiagonalLine<coordType>(Point_a, Point_b, Color, "back");
+  Point<coordType> Point_c {xa+(xb-xa), ya};
+  Point<coordType> Point_d {xb+(xb-xa), yb};
+  DiagonalLine<coordType>(Point_c, Point_d, Color, "front");
   for(auto dx=xb-(xb-xa);dx < xb+(yb-ya);++dx){
     DrawPixel(dx, yb, Color);//Horiz bot
   }
 }
 
 template<typename coordType>
-void OutlineParallelogram(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b,
+void OutlineParallelogram(Point<coordType>& Point_a, Point<coordType>& Point_b,
                           u32& Color){
-  auto xa = Vec_a.getX(), ya = Vec_a.getY(), xb = Vec_b.getX(), yb = Vec_b.getY();
-  Vec2<coordType> Vec_c {xb,ya};
-  Vec2<coordType> Vec_d {xa+xb,yb};
-    DiagonalLine<coordType>(Vec_a, Vec_b, Color, "front");
-    DiagonalLine<coordType>(Vec_c, Vec_d, Color, "front");
-    DiagonalLine<coordType>(Vec_a, Vec_b, Color, "back");
-    DiagonalLine<coordType>(Vec_c, Vec_d, Color, "back");
+  auto xa = Point_a.getX(), ya = Point_a.getY(), xb = Point_b.getX(), yb = Point_b.getY();
+  Point<coordType> Point_c {xb,ya};
+  Point<coordType> Point_d {xa+xb,yb};
+    DiagonalLine<coordType>(Point_a, Point_b, Color, "front");
+    DiagonalLine<coordType>(Point_c, Point_d, Color, "front");
+    DiagonalLine<coordType>(Point_a, Point_b, Color, "back");
+    DiagonalLine<coordType>(Point_c, Point_d, Color, "back");
     for(auto dx=xa, dx_=xb;dx < xb;++dx,++dx_){
       DrawPixel(dx, ya, Color); //Horiz top
       DrawPixel(dx_, yb, Color);//Horiz bot
@@ -217,8 +218,8 @@ void ClearScreen(u32 Color) {
 }
 
 template<typename coordType>
-void OutlineGrid(Vec2<coordType>& Vec_a, Vec2<coordType>& Vec_b, int& cell_size,int& col_row_cell_n, u32& Color){
-  auto xa = Vec_a.getX(), ya = Vec_a.getY(), xb = Vec_b.getX(), yb = Vec_b.getY();
+void OutlineGrid(Point<coordType>& Point_a, Point<coordType>& Point_b, int& cell_size,int& col_row_cell_n, u32& Color){
+  auto xa = Point_a.getX(), ya = Point_a.getY(), xb = Point_b.getX(), yb = Point_b.getY();
   for(auto x = xa;x < xb;++x){
     for(auto dy=ya,alt=0;dy < yb;dy=dy+cell_size,++alt){
       if(alt % col_row_cell_n == 0){
@@ -257,7 +258,6 @@ LRESULT CALLBACK WindowProc(HWND Window, UINT Message, WPARAM WParam, LPARAM LPa
 }
 
 int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PWSTR CmdLine, int CmdShow) {
-
     WNDCLASS WindowClass = {};
     const wchar_t ClassName[] = L"MyWindowClass";
     WindowClass.lpfnWndProc = WindowProc;
@@ -279,8 +279,8 @@ int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PWSTR CmdLine, i
     // Get client area dimensions
     RECT ClientRect;
     GetClientRect(Window, &ClientRect);
-    ClientWidth = (ClientRect.right*2) + (ClientRect.left*2);
-    ClientHeight = (ClientRect.bottom*2) + (ClientRect.top*2);
+    ClientWidth = (ClientRect.right*2) + (ClientRect.left*2); //Double the width
+    ClientHeight = (ClientRect.bottom*2) + (ClientRect.top*2);//Double the height
 
     BitmapWidth = ClientWidth;
     BitmapHeight = ClientHeight;
@@ -294,7 +294,7 @@ int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PWSTR CmdLine, i
     BITMAPINFO BitmapInfo;
     BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
     BitmapInfo.bmiHeader.biWidth = BitmapWidth;
-    // Negative BitmapHeight makes top left as the coordinate system origin for the DrawPixel function, otherwise its bottom left
+    // Negative BitmapHeight makes top left as the coordinate system origin(0,0), otherwise its bottom left
     BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
     BitmapInfo.bmiHeader.biPlanes = 1;
     BitmapInfo.bmiHeader.biBitCount = 32;
@@ -304,17 +304,16 @@ int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PWSTR CmdLine, i
 
     bool Running = true;
 
-    //Grid Setup/Colors
-    int cell_size = 20;
-    int col_row_cell_n = 5;
-    //padding offset for grid, in px
+    //Grid Setup
+    int cell_size = 10;//sets pixel count of each cell(grid unit)
+    int col_row_cell_n = 5;//sets the number of cells inside each col/row
+    //padding offset for grid rectangle corners(in pixels)
+    //offsets are needed or else bitmap program crashes
     auto grid_pad_left = 1, grid_pad_top = 1;
-    typedef Vec2<int> Vec2int;//using integers for coordinates
-    Vec2int grid_top{grid_pad_left, grid_pad_top};
-    Vec2int grid_bot{1950,1050};
+    using PointInt = Point<int>;//using integers for coordinates
+    PointInt grid_top{grid_pad_left, grid_pad_top};
+    PointInt grid_bot{1920,1080};//make a 1080 resolution grid
 
-    //Vec2int grid_bot{ClientWidth/2, ClientHeight/2};//w, h
-    // Vec2int grid_top{0,1};
     while(Running) {
         MSG Message;
         while(PeekMessage(&Message, nullptr, 0, 0, PM_REMOVE)) {
@@ -322,46 +321,49 @@ int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PWSTR CmdLine, i
             TranslateMessage(&Message);
             DispatchMessage(&Message);
         }
+
         //Setup the grid/background/point of origin
-        ClearScreen(offwhite);
-        /*        Function Call        */
-        OutlineGrid(grid_top, grid_bot, cell_size, col_row_cell_n, cyan_lite);
+        ClearScreen(0xFFFFFF);
+        OutlineGrid(grid_top, grid_bot, cell_size, col_row_cell_n, grey_lite);
 
         //For accurate cartesian coordinate view, the axis origin points can be offset for even columns
         auto origin_axis = [&](int&& axis_len){
           auto ax_len_offset = axis_len % (cell_size*col_row_cell_n);
           auto cols_ = static_cast<int>((axis_len-ax_len_offset)/(cell_size*col_row_cell_n));
-          auto ret_ = (cell_size*col_row_cell_n)*static_cast<int>(cols_/2);
-          return ret_;
+          auto fixed_origin = (cell_size*col_row_cell_n)*static_cast<int>(cols_/2);
+          return fixed_origin;
         };
+
         //Drawing the axis lines without an offset
-        //Maybe we can just cut off the offset or change the clients window resolution?
+        //Maybe we can just `clamp` the width/height of each axis line
         auto org_x = origin_axis(grid_bot.getX()-grid_top.getX());
         auto org_y = origin_axis(grid_bot.getY()-grid_top.getY());
-        auto x_radius = static_cast<int>(org_x);//-(cell_size*col_row_cell_n));
-        auto y_radius = static_cast<int>(org_y);//-(cell_size*col_row_cell_n));
-        HLine_expand(org_x, org_y, x_radius, green);//x-axis
-        VLine_expand(org_x, org_y, y_radius, green);//y-axis
+        auto x_axis_line_length = (org_x-(cell_size*col_row_cell_n))*2;
+        auto y_axis_line_length = (org_y-(cell_size*col_row_cell_n))*2;
+        HLine_expand(org_x, org_y, x_axis_line_length, green);//x-axis line
+        VLine_expand(org_x, org_y, y_axis_line_length, green);//y-axis line
 
         //TESTING PURPOSE
         //-------------------------------
-        //Vec2int test = {0, 0};
-        //test.set_ndc(origin_x, origin_y, cell_size);
-        //assert the ndc x and y (0,0) is at the now device coordinate's origin
-        //assert(test.getX() == origin_x);
-        //assert(test.getY() == origin_y);
+        // Assert the ndc's origin Point is set correctly, relative to ClientWindows width/height
+        PointInt test = {0, 0};
+        test.set_ndc(org_x, org_y, cell_size);
+        assert(test.getX() == org_x);
+        assert(test.getY() == org_y);
         //-------------------------------
 
         //Sample of functions
+        //-------------------------------
         //top = xa,ya and bot = ya,yb // these coordinates are top left and bottom right
-        //Now using NDC coordinates
-        //Vec2int top_dc = {(BitmapWidth/4)-15, (BitmapHeight/6)+80};
-        //Vec2int bot_dc = {(BitmapWidth/2)-30, (top_dc.getX() + top_dc.getY())};
-        Vec2int top { -20, -20 };
-        Vec2int bot {  10,  10 };
-        //top.set_ndc(origin_x, origin_y, cell_size);
-        //bot.set_ndc(origin_x, origin_y, cell_size);
-        //FillSquare        (top, bot, bluegrey);        /*        ->   "|#|" */
+        //Instead of using Device Coordinates:
+        //PointInt top_dc = {(BitmapWidth/4)-15, (BitmapHeight/6)+80};
+        //PointInt bot_dc = {(BitmapWidth/2)-30, (top_dc.getX() + top_dc.getY())};
+        //Use cartesian coordinates, `normalized` DC:
+        PointInt top { -10, -10 };
+        PointInt bot {  10,  10 };
+        top.set_ndc(org_x, org_y, cell_size);
+        bot.set_ndc(org_x, org_y, cell_size);
+        // FillSquare(top, bot, bluegrey);        /*        ->   "|#|" */
         // OutlineSquare     (top, bot, purp_lite);                /*        ->   "|_|" */
         // VertexPointSquare (top, bot, purp_lite);                /*        ->   ": :" */
         // DiagonalLine      (top, bot, purp_lite, "front");       /*        ->    "\"  */
@@ -371,11 +373,11 @@ int WINAPI wWinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PWSTR CmdLine, i
         // OutlineRightTriangle(top, bot, purp_lite, false, true); /*        ->   "/|"  */
         // OutlineRightTriangle(top, bot, purp_lite, true, true);  /*        ->   "\|"  */
         // OutlineParallelogram(top, bot, purp_lite);      /*        ->   "\\"  */
-        // OutlineEquilTriangle(top, bot, purp_lite, false);       /*        ->   "/\"  */
+        OutlineEquilTriangle(top, bot, purp_lite, false);       /*        ->   "/\"  */
+        //-------------------------------
+        
         StretchDIBits(DeviceContext,0, 0,BitmapWidth, BitmapHeight,0, 0,ClientWidth, ClientHeight,
                       BitmapMemory, &BitmapInfo,DIB_RGB_COLORS, SRCCOPY);
     }
-
-
     return 0;
 }
